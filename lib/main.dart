0117b99b3c4525949593d94f608afbce553cb676
phoenix <habibiyan@outlook.com>
2022-04-08 21:26:50 +0430

    draw line

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dot Line Game',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
@@ -30,22 +30,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var _dots = List<int>.filled(12, 0);
-  var _lines = List<Line>.empty();
+  var _lines = new List<Line>.empty(growable: true);
 
   int _startRow = -3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Test"),
+          title: const Text("Dot Line Game"),
         ),
-        body: RenderBoard());
+        body: renderBoard());
   }
 
-  Widget RenderBoard() {
+  Widget renderBoard() {
+    _startRow = -3;
     final children = <Widget>[];
     for (var i = 0; i < 4; i++) {
-      children.add(RenderRow());
+      children.add(renderRow());
     }
     return Center(
       child: Column(
@@ -55,23 +56,33 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget RenderRow() {
+  Widget renderRow() {
     setState(() {
       _startRow += 3;
     });
 
     final children = <Widget>[];
-    for (var j = _startRow; j < _startRow + 3; j++) {
+    for (var dotIndex = _startRow; dotIndex < _startRow + 3; dotIndex++) {
       children.add(Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
             children: [
-              if (j == 1 || j == 4 || j == 7 || j == 10)
-                Container(
-                  width: MediaQuery.of(context).size.width / 4,
-                  height: 10,
-                  color: Colors.white,
+              if (dotIndex == 1 ||
+                  dotIndex == 4 ||
+                  dotIndex == 7 ||
+                  dotIndex == 10)
+                InkWell(
+                  onTap: () => {drawHorizontalLine(dotIndex - 1)},
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: 10,
+                    color: _lines.any((x) =>
+                            x.source == dotIndex - 1 &&
+                            x.destination == dotIndex)
+                        ? Colors.blueAccent
+                        : Colors.white,
+                  ),
                 ),
               Container(
                 alignment: Alignment.center,
@@ -79,25 +90,41 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 50,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(50),
-                    color: _dots.length > j && _dots[j] == 0
+                    color: _dots.length > dotIndex && _dots[dotIndex] == 0
                         ? Colors.grey
-                        : _dots.length > j && _dots[j] == 1
+                        : _dots.length > dotIndex && _dots[dotIndex] == 1
                             ? Colors.blueAccent
                             : Colors.redAccent),
               ),
-              if (j == 1 || j == 4 || j == 7 || j == 10)
-                Container(
-                  width: MediaQuery.of(context).size.width / 4,
-                  height: 10,
-                  color: Colors.white,
+              if (dotIndex == 1 ||
+                  dotIndex == 4 ||
+                  dotIndex == 7 ||
+                  dotIndex == 10)
+                InkWell(
+                  onTap: () => {drawHorizontalLine(dotIndex)},
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: 10,
+                    color: _lines.any((x) =>
+                            x.source == dotIndex &&
+                            x.destination == dotIndex + 1)
+                        ? Colors.blueAccent
+                        : Colors.white,
+                  ),
                 )
             ],
           ),
-          if (j < 9)
-            Container(
-              width: 10,
-              height: MediaQuery.of(context).size.width / 4,
-              color: Colors.white,
+          if (dotIndex < 9)
+            InkWell(
+              onTap: () => {drawVerticalLine(dotIndex)},
+              child: Container(
+                width: 10,
+                height: MediaQuery.of(context).size.width / 4,
+                color: _lines.any((x) =>
+                        x.source == dotIndex && x.destination == dotIndex + 3)
+                    ? Colors.blueAccent
+                    : Colors.white,
+              ),
             ),
         ],
       ));
@@ -108,9 +135,31 @@ class _MyHomePageState extends State<MyHomePage> {
       children: children,
     );
   }
+
+  void drawVerticalLine(int dotIndex) {
+    if (_lines
+        .any((x) => x.source == dotIndex && x.destination == dotIndex + 3)) {
+      return;
+    }
+
+    print("dot ${dotIndex}");
+    _lines.add(Line(dotIndex, dotIndex + 3));
+    renderBoard();
+  }
+
+  void drawHorizontalLine(int dotIndex) {
+    if (_lines
+        .any((x) => x.source == dotIndex && x.destination == dotIndex + 1)) {
+      return;
+    }
+
+    print("dot ${dotIndex}");
+    _lines.add(Line(dotIndex, dotIndex + 1));
+    renderBoard();
+  }
 }
 
 class Line {
-  int source = 0;
-  int destination = 0;
+  int source, destination;
+  Line(this.source, this.destination) {}
 }
